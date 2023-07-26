94fd268a7846a8e8d4e2e98c8e9695d8115b1ede
xcasadio <casadprod@hotmail.fr>
2023-07-26 21:06:15 +0200

    update DemoGame.PlayerComponent

--- DemosGame/PlayerComponent.cs
+++ DemosGame/PlayerComponent.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Text.Json;
 using CasaEngine.Core.Shapes;
 using CasaEngine.Framework.Entities;
@@ -32,29 +34,44 @@ public class PlayerComponent : Component
 
     public override void Update(float elapsedTime)
     {
-        var velocity = 50f;
+        var velocity = 80f;
         var velocityX = 0f;
         var velocityY = 0f;
+        string animationName = "";
 
         if (Keyboard.GetState().IsKeyDown(Keys.Down))
         {
             velocityY = -velocity;
+            animationName = "swordman_walk_down";
         }
         else if (Keyboard.GetState().IsKeyDown(Keys.Up))
         {
             velocityY = velocity;
+            animationName = "swordman_walk_up";
         }
 
         if (Keyboard.GetState().IsKeyDown(Keys.Right))
         {
             velocityX = velocity;
+            animationName = "swordman_walk_right";
         }
         else if (Keyboard.GetState().IsKeyDown(Keys.Left))
         {
             velocityX = -velocity;
+            animationName = "swordman_walk_left";
         }
 
         _physics2dComponent.Velocity = new Vector2(velocityX, velocityY);
+
+        if (!string.IsNullOrWhiteSpace(animationName) && !animationName.Equals(_animatedSpriteComponent.CurrentAnimation.Animation2dData.Name))
+        {
+            Debug.WriteLine(DateTime.Now.TimeOfDay + " " + animationName + " " + _animatedSpriteComponent.CurrentAnimation.Animation2dData.Name);
+            _animatedSpriteComponent.SetCurrentAnimation(animationName, true);
+        }
+        else if (string.IsNullOrWhiteSpace(animationName) && !_animatedSpriteComponent.CurrentAnimation.Animation2dData.Name.Contains("stand"))
+        {
+            _animatedSpriteComponent.SetCurrentAnimation(_animatedSpriteComponent.CurrentAnimation.Animation2dData.Name.Replace("walk", "stand"), true);
+        }
 
         if (Keyboard.GetState().IsKeyDown(Keys.Add))
         {
