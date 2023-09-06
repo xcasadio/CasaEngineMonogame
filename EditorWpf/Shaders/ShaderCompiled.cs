5349e40f306fadb9357fcdccd8c5ce998a1d266a
xcasadio <casadprod@hotmail.fr>
2023-09-06 13:35:28 +0200

    Editor : can compile shader on the fly

---
+++ EditorWpf/Shaders/ShaderCompiled.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace EditorWpf.Shaders;
+
+internal class ShaderCompiled
+{
+    public List<string> Dependencies { get; } = new();
+
+    public byte[]? ByteCode { get; set; }
+
+    public void AddDependency(string file)
+    {
+        Dependencies.Add(file);
+    }
+}
