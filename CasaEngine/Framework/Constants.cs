8bcb5efdd1c5318807cab802ba54ef15a446af65
xcasadio <casaprod@hotmail.fr>
2026-02-25 22:58:43 +0100

    Task 1.4: Add StaticModel file extension constant

--- CasaEngine/Framework/Constants.cs
+++ CasaEngine/Framework/Constants.cs
@@ -17,6 +17,7 @@ public static class Constants
         public const string TileMap = ".tileMap";
         public const string Screen = ".screen";
         public const string Model = ".model";
+        public const string StaticModel = ".staticModel";
         public const string SkeletonAnimation = ".skeletonAnim";
         public const string Material = ".material";
         public const string Shader = ".shader";
